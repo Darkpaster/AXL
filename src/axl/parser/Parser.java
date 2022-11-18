3bd462792791b5bcf968037568cc58c61ef53f56
Anton Kochergin <87330809+kesha2327@users.noreply.github.com>
2022-11-18 22:07:08 +0400

    Create Parser.java

--- src/axl/parser/Parser.java
+++ src/axl/parser/Parser.java
@@ -1,5 +1,24 @@
 package axl.parser;
 
+import axl.parser.ast.*;
+import axl.lexer.*;
+
 public class Parser
 {
+    private ArrayList<Token> tokens;
+    private Token current;
+    private int i;
+
+    public Parser(ArrayList<Token> tokens)
+    {
+        this.i = 0;
+        this.tokens = tokens;
+        this.current = next();
+    }
+
+    public void next()
+    {
+        if(i < this.tokens.size())
+
+    }
 }
